3b7cf9236c80f246167ddc58c97d0179780f73bb
Bishalis <shahibishal786526@gmail.com>
2025-09-13 22:32:20 +1000

    DB bug fix

--- Backend/index.js
+++ Backend/index.js
@@ -14,20 +14,12 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Middleware for parsing request body
+
 app.use(express.json());
 
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
+
 app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+
 
 // Serve static files from uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
